e5fec30f6da60a66e9fc87d5d3bc4a3d4e9a6c3e
Domistro16 <140549689+Domistro16@users.noreply.github.com>
2025-04-15 23:21:55 +0100

    Update BaseRegistrarImplementation.sol

--- contracts/ethregistrar/BaseRegistrarImplementation.sol
+++ contracts/ethregistrar/BaseRegistrarImplementation.sol
@@ -32,15 +32,13 @@ contract BaseRegistrarImplementation is ERC721, IBaseRegistrar, Ownable {
     bytes4 private constant RECLAIM_ID =
         bytes4(keccak256("reclaim(uint256,address)"));
 
-    /**
-     * v2.1.3 version of _isApprovedOrOwner which calls ownerOf(tokenId) and takes grace period into consideration instead of ERC721.ownerOf(tokenId);
-     * https://github.com/OpenZeppelin/openzeppelin-contracts/blob/v2.1.3/contracts/token/ERC721/ERC721.sol#L187
-     * @dev Returns whether the given spender can transfer a given token ID
-     * @param spender address of the spender to query
-     * @param tokenId uint256 ID of the token to be transferred
-     * @return bool whether the msg.sender is approved for the given token ID,
-     *    is an operator of the owner, or is the owner of the token
-     */
+    /// v2.1.3 version of _isApprovedOrOwner which calls ownerOf(tokenId) and takes grace period into consideration instead of ERC721.ownerOf(tokenId);
+    /// https://github.com/OpenZeppelin/openzeppelin-contracts/blob/v2.1.3/contracts/token/ERC721/ERC721.sol#L187
+    /// @dev Returns whether the given spender can transfer a given token ID
+    /// @param spender address of the spender to query
+    /// @param tokenId uint256 ID of the token to be transferred
+    /// @return bool whether the msg.sender is approved for the given token ID,
+    ///              is an operator of the owner, or is the owner of the token
     function _isApprovedOrOwner(
         address spender,
         uint256 tokenId
@@ -54,6 +52,7 @@ contract BaseRegistrarImplementation is ERC721, IBaseRegistrar, Ownable {
     constructor(ENS _ens, bytes32 _baseNode) ERC721("", "") {
         ens = _ens;
         baseNode = _baseNode;
+        Ownable(msg.sender);
     }
 
     modifier live() {
@@ -66,12 +65,10 @@ contract BaseRegistrarImplementation is ERC721, IBaseRegistrar, Ownable {
         _;
     }
 
-    /**
-     * @dev Gets the owner of the specified token ID. Names become unowned
-     *      when their registration expires.
-     * @param tokenId uint256 ID of the token to query the owner of
-     * @return address currently marked as the owner of the given token ID
-     */
+    /// @dev Gets the owner of the specified token ID. Names become unowned
+    ///      when their registration expires.
+    /// @param tokenId uint256 ID of the token to query the owner of
+    /// @return address currently marked as the owner of the given token ID
     function ownerOf(
         uint256 tokenId
     ) public view override(IERC721, ERC721) returns (address) {
@@ -107,26 +104,22 @@ contract BaseRegistrarImplementation is ERC721, IBaseRegistrar, Ownable {
         return expiries[id] + GRACE_PERIOD < block.timestamp;
     }
 
-    /**
-     * @dev Register a name.
-     * @param id The token ID (keccak256 of the label).
-     * @param owner The address that should own the registration.
-     * @param duration Duration in seconds for the registration.
-     */
+    /// @dev Register a name.
+    /// @param id The token ID (keccak256 of the label).
+    /// @param owner The address that should own the registration.
+    /// @param duration Duration in seconds for the registration.
     function register(
         uint256 id,
         address owner,
         uint256 duration
-    ) external override returns (uint256) {
+    ) external override virtual returns (uint256) {
         return _register(id, owner, duration, true);
     }
 
-    /**
-     * @dev Register a name, without modifying the registry.
-     * @param id The token ID (keccak256 of the label).
-     * @param owner The address that should own the registration.
-     * @param duration Duration in seconds for the registration.
-     */
+    /// @dev Register a name, without modifying the registry.
+    /// @param id The token ID (keccak256 of the label).
+    /// @param owner The address that should own the registration.
+    /// @param duration Duration in seconds for the registration.
     function registerOnly(
         uint256 id,
         address owner,
@@ -165,7 +158,7 @@ contract BaseRegistrarImplementation is ERC721, IBaseRegistrar, Ownable {
     function renew(
         uint256 id,
         uint256 duration
-    ) external override live onlyController returns (uint256) {
+    ) external override virtual live onlyController returns (uint256) {
         require(expiries[id] + GRACE_PERIOD >= block.timestamp); // Name must be registered here or in grace period
         require(
             expiries[id] + duration + GRACE_PERIOD > duration + GRACE_PERIOD
@@ -176,9 +169,7 @@ contract BaseRegistrarImplementation is ERC721, IBaseRegistrar, Ownable {
         return expiries[id];
     }
 
-    /**
-     * @dev Reclaim ownership of a name in ENS, if you own it in the registrar.
-     */
+    /// @dev Reclaim ownership of a name in ENS, if you own it in the registrar.
     function reclaim(uint256 id, address owner) external override live {
         require(_isApprovedOrOwner(msg.sender, id));
         ens.setSubnodeOwner(baseNode, bytes32(id), owner);
@@ -192,4 +183,9 @@ contract BaseRegistrarImplementation is ERC721, IBaseRegistrar, Ownable {
             interfaceID == ERC721_ID ||
             interfaceID == RECLAIM_ID;
     }
+
+    function _exists(uint256 tokenId) internal view override returns (bool) {
+        return super._exists(tokenId);
+    }
+
 }
