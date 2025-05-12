03d2f0b22d50318f5004e50102dac3a1543b8f11
domistro <desmondesih@gmail.com>
2025-05-12 18:51:08 +0100

    new messages

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,22 +1,27 @@
 import react from '@vitejs/plugin-react'
 import { defineConfig, loadEnv } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
-import { NodeGlobalsPolyfillPlugin } from '@esbuild-plugins/node-globals-polyfill'
+import { nodePolyfills } from 'vite-plugin-node-polyfills'
 // https://vitejs.dev/config/
 export default defineConfig({
-   define: { global: 'globalThis',
-               'process.env': {},
-    },
+/*   define: { global: 'globalThis', 'process.env': {} },
   resolve: { alias: { buffer: 'buffer/' } },
   optimizeDeps: {
     include: ['buffer'],
     esbuildOptions: {
-      plugins: [
-        NodeGlobalsPolyfillPlugin({ buffer: true })
-      ]
-    }
-  },
+      plugins: [NodeGlobalsPolyfillPlugin({ buffer: true })],
+    },
+  }, */
   plugins: [
+    nodePolyfills({
+      // enable Buffer & process globals
+      globals: {
+        Buffer: true,
+        process: true,
+      },
+      // allow imports like "import fs from 'fs'" if you ever need them
+      protocolImports: true,
+    }),
     react(),
     tailwindcss(),
     ,
