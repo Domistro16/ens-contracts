cabac17a960be7a6edb8d3b76272c591c80a1f78
Desmond Egwurube <desmondesih@gmail.com>
2025-07-12 09:41:56 +0100

    edited the login procedure

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,5 +1,5 @@
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
-import React, { useEffect, useRef, useState } from 'react'
+import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import '@rainbow-me/rainbowkit/styles.css'
@@ -31,7 +31,7 @@ function BootStrap() {
   const web3authContextConfig: Web3AuthContextConfig = {
     web3AuthOptions: web3AuthOptions,
   }
-  const [synced, setSynced] = useState(false)
+ /*  const [synced, setSynced] = useState(false)
   const iframeRef = useRef<HTMLIFrameElement>(null)
 
   useEffect(() => {
@@ -70,7 +70,7 @@ function BootStrap() {
         title="session-sync"
       />
     )
-  }
+  } */
   return (
     <Web3AuthProvider config={web3authContextConfig}>
       <QueryClientProvider client={queryClient}>
